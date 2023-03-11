f2078d83874965028c36ee830cf751ed9f1cb4aa
Rohit Nittawadekar <124390109+Rohit3920@users.noreply.github.com>
2023-03-10 19:48:27 -0800

    default function call

--- MainModule.js
+++ MainModule.js
@@ -1,9 +1,10 @@
 // import { r as rohit, yuh, obj } from "./labraryModule.js";
-import * as mod  from "./labraryModule.js";
+// import * as mod  from "./labraryModule.js";
+import {default as mod}  from "./labraryModule.js";
 
 
 console.log( mod);
 
-mod.yuh(9,7,3,3);
+mod(9,7,3,3);
 
 console.log(mod);
--- labraryModule.js
+++ labraryModule.js
@@ -1,6 +1,6 @@
 export let r = "Hello, I'm Rohit from Web Developer"
 
-export function yuh(a,b,d,e){
+export default function yuh(a,b,d,e){
     let c = a+b+d+e;
     console.log(c);    
 }
