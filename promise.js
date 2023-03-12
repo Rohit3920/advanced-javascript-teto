670cdbb58c7c37c30c2f2eddf8610f8b3840f8b3
Rohit Nittawadekar <124390109+Rohit3920@users.noreply.github.com>
2023-03-11 19:49:03 -0800

    main Example for promise

--- promise.js
+++ promise.js
@@ -1,17 +1,42 @@
 // document.write("<h2>I'm Web Devaloper ROHIT</h2>")
 
 
-let con = true;
+// let con = true;
 
-let prom = new Promise( function(resolve, reject){
+// let prom = new Promise( function(resolve, reject){
 
-    if(con){
-        resolve("I am successfull.");
-    }else{
-        reject("I am Reject.");
-    }
-});
+//     if(con){
+//         resolve("I am successfull.");
+//     }else{
+//         reject("I am Reject.");
+//     }
+// });
 
-console.log(prom);
+// console.log(prom);
 
 
+let complate = true;
+
+function prom( complete ){
+    return new Promise(function(resolve, reject){
+        if(complete){
+            resolve("I am successfull.");
+        }else{
+            reject("I am Failed");
+        }
+    });
+}
+
+let onFulfilment = (result) => {
+    console.log(result);
+    document.write(result);
+}
+
+let onRejection = (error) => {
+    console.log(error);
+    document.write(error);
+}
+
+
+prom(complate).then(onFulfilment);
+prom(complate).catch(onRejection);
